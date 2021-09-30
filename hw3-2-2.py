55550b739775f5d103dd0aed712728afd879e4a7
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-09-30 10:32:16 -0400

    Finish hw 3-2-2

--- hw3-2-2.py
+++ hw3-2-2.py
@@ -1,19 +1,17 @@
 # Author: MOG 9/29/21
 
 height = input("How tall are you in centimeters? ")
-weight = ("How much do you weigh in kilograms? ")
+weight = input("How much do you weigh in kilograms? ")
 
 bmi = (float(weight) / float(height) ** 2) * 10000
 
-if bmi > 19:
-    print("")
-elif bmi > 25:
-    print("")
-elif bmi > 30:
-    print("")
-elif bmi > 35:
-    print("")
-elif bmi > 40:
-    print("")
+if bmi < 19:
+    print("You are underweight with a bmi of " + str(bmi))
+elif bmi < 25:
+    print("You are healthy with a bmi of " + str(bmi))
+elif bmi < 30:
+    print("You are overweight with a bmi of " + str(bmi))
+elif bmi < 35:
+    print("You are obese with a bmi of " + str(bmi))
 else:
-    print("")
+    print("You are extremely obese with a bmi of " + str(bmi))
